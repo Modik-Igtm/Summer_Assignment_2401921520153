9d99cefff47affad10b7694f2245a0172a6baf20
Modika Sachan <24112CN020@glbitm.ac.in>
2026-06-06 16:33:23 +0530

    best time to buy and sell stock

---
+++ week_1/day_1/P3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min=prices[0],max=0;
+        for(int i=0;i<prices.size();i++){
+            if(prices[i]<min)
+            min=prices[i];
+            else
+        { int profit=prices[i]-min;
+
+        if(profit>max)
+        
+            max=profit;
+        
+        }}
+        return max;
+    }
+    
+};
