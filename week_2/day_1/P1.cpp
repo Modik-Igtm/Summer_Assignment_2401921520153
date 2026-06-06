8d28cbabf628b8707cf1f3145191c8be3c62d7cd
Modika Sachan <24112CN020@glbitm.ac.in>
2026-06-06 17:30:22 +0530

    changes

---
+++ week_2/day_1/P1.cpp
@@ -0,0 +1,17 @@
+//Valid Anagram
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()) return false;
+        vector<int>freq(26,0);
+        for(char c: s) freq[c-'a']++;
+        for (char c:t) freq[c-'a']--;
+        for(int i:freq)
+        {
+            if(i!=0) return false;
+        }
+        return true;
+    }
+};
